45092f0d49b82f2e5cc3a76540e992ca8bf1ad32
Peruere1828 <fmyh1828@gmail.com>
2026-03-15 10:39:41 +0000

    实现STAGE1 REQ1-2

--- Code/config.h
+++ Code/config.h
@@ -6,9 +6,9 @@
 // #define DEBUG 1
 
 // 识别八进制和十六进制整数
-// #define STAGE_ONE_REQ_ONE 1
+#define STAGE_ONE_REQ_ONE 1
 // 识别科学记数法小数
-// #define STAGE_ONE_REQ_TWO 1
+#define STAGE_ONE_REQ_TWO 1
 // 识别单行注释和跨行注释
 // #define STAGE_ONE_REQ_THREE 1
 
